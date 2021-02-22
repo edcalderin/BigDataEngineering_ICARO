cc716bdca674c78919097849eaf8bf4651217b15
Erick <edcm.erick@gmail.com>
2021-02-22 16:35:40 +0000

    greeting sh

---
+++ ShellScripting/Greeting.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+greeting="Bienvenido"
+user=$(whoami)
+day=$(date +%A)
+
+echo "$greeting $user! Hoy es $day, y es el mejor dia de la semana!"
+echo "Tu version de bash es la siguiente: $BASH_VERSION."
